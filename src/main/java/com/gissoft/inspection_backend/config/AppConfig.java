dacf91c7234350dc36861fab17b7925cc56a7db3
Sachin Advait <sachinadvaita@gmail.com>
2026-03-15 18:16:14 +0530

    Updated the properties file.

--- src/main/java/com/gissoft/inspection_backend/config/AppConfig.java
+++ src/main/java/com/gissoft/inspection_backend/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.gissoft.inspection_backend.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,15 +10,22 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(
-                ObjectUtils.asMap(
-                        "cloud_name", "dxv3dts8k",
-                        "api_key", "422744849866437",
-                        "api_secret", "G3im-z914VaIXMdpuIIIw8BK3vM"
-                )
-        );
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
     }
 
     @Bean
